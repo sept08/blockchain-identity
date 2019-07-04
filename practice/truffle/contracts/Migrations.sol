e80d6321e44fd09b5348fe55ffcac9418109dc2c
tianjiaqi <tianjiaqi@youzan.com>
2019-07-04 09:37:09 +0800

    comment for migration

--- practice/truffle/contracts/Migrations.sol
+++ practice/truffle/contracts/Migrations.sol
@@ -7,77 +7,61 @@ contract myToken {
     uint8 public constant decimals = 18;  // 18 is the most common number of decimal places
     uint _totalSupply;
 
-    // Balances for each account stored using a mapping
+    // 使用mapping存储每个账户的余额
     mapping(address => uint256) balances;
 
-    // Owner of the account approves the allowance of another account
-    // Create an allowance mapping
-    // The first key is the owner of the tokens
-    // In the 2nd mapping, its says who can spend on your behalf, and how many
-    // So, we are creating a mapping, where the kep is an address,
-    // The value is further a mapping of address to amount
+    // 账户的限额关系
     mapping(address => mapping (address => uint256)) allowance;
 
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 
-
-    // Called automatically when contract is initiated
-    // Sets to total initial _totalSupply, as per the input argument
-    // Also gives the initial supply to msg.sender...who creates the contract
     constructor(uint amount) public {
         _totalSupply = amount;
         balances[msg.sender] = amount;
     }
 
-    // Returns the total supply of tokens
+    // 代币的总储量
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
-    // Get the token balance for account `tokenOwner`
-    // Anyone can query and find the balance of an address
+    // 根据代币所有者的地址，查询其代币余额。
     function balanceOf(address tokenOwner) public constant returns (uint balance) {
         return balances[tokenOwner];
     }
 
-    // Transfer the balance from owner's account to another account
-    // Decreases the balance of "from" account
-    // Increases the balance of "to" account
-    // Emits Transfer event
+    // 由账户所有者向另一账户进行代币转账
     function transfer(address to, uint tokens) public returns (bool success) {
+        // 代币不足的异常处理
         if(tokens < 1){
             revert("Not enough Ether provided.");
         }
+        // 转账的代币数量应当小于等于当前账户的余额，否则
         require(tokens <= balances[msg.sender]);
         balances[msg.sender] = balances[msg.sender] - tokens;
         balances[to] = balances[to] + tokens;
+        // 触发转账事件
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
 
-    // Send amount of tokens from address `from` to address `to`
-    // The transferFrom method is used to allow contracts to spend
-    // tokens on your behalf
-    // Decreases the balance of "from" account
-    // Decreases the allowance of "msg.sender"
-    // Increases the balance of "to" account
+    // 发送一定数量的代币
+    // The transferFrom method is used to allow contracts to spend tokens on your behalf
     // Emits Transfer event
     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
         balances[from] = balances[from] - tokens;
         allowance[from][msg.sender] = allowance[from][msg.sender] - tokens;
         balances[to] = balances[to] + tokens;
+        // 触发转账事件
         emit Transfer(from, to, tokens);
         return true;
     }
 
-    // Approves the `spender` to withdraw from your account, multiple times, up to the `tokens` amount.
-    // So the msg.sender is approving the spender to spend these many tokens
-    // from msg.sender's account
-    // Setting up allowance mapping accordingly
-    // Emits approval event
+    // 批准 spender 多次从您的账户中提款，直到达到限额 tokens。
     function approve(address spender, uint tokens) public returns (bool success) {
         allowance[msg.sender][spender] = tokens;
+        // 触发授权批准事件
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
