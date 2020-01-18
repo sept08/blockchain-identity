18370901456cf6c5a219919084c5aa5aa7e2d761
tian <littlejixing@163.com>
2020-01-18 15:17:05 +0800

    deploy contract

--- project/sixth/project-6/migrations/2_deploy_contracts.js
+++ project/sixth/project-6/migrations/2_deploy_contracts.js
@@ -1,14 +1,12 @@
 // migrating the appropriate contracts
-var FarmerRole = artifacts.require("./FarmerRole.sol");
-var DistributorRole = artifacts.require("./DistributorRole.sol");
+var ManufacturerRole = artifacts.require("./ManufacturerRole.sol");
 var RetailerRole = artifacts.require("./RetailerRole.sol");
-var ConsumerRole = artifacts.require("./ConsumerRole.sol");
+var CustomerRole = artifacts.require("./CustomerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(FarmerRole);
-  deployer.deploy(DistributorRole);
+  deployer.deploy(ManufacturerRole);
   deployer.deploy(RetailerRole);
-  deployer.deploy(ConsumerRole);
+  deployer.deploy(CustomerRole);
   deployer.deploy(SupplyChain);
 };
