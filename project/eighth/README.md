1b23c200e1be404a050fb1c151ac5249aa8187f5
tianjiaqi <tianjiaqi@youzan.com>
2020-01-19 18:58:52 +0800

    update proj

--- project/eighth/README.md
+++ project/eighth/README.md
@@ -2,6 +2,24 @@
 
 The capstone will build upon the knowledge you have gained in the course in order to build a decentralized housing product. 
 
+# How to mint a new token without verification (Only for testing)
+Run the following in the truffle console
+`for(let i=1;i<=10;i++){SolnSquareVerifier.deployed().then(e=>e.mint("0x4b2c95D63a707824F896AB14027Ff0085925553D",i,{from:"0x4b2c95D63a707824F896AB14027Ff0085925553D"})).then(e=>{console.log(e)}).catch(e=>{console.log(e)});}`
+
+# Truffle Test
+- Run `truffle test` to test the code.
+
+# Rinkeby Contract Address
+First the `Verifier` contract is deployed then the `SolnSquareVerifier` contract using the `Verifier` address.
+- Verifier - https://rinkeby.etherscan.io/address/0xaD169D65827090661C40aB5dd9Ad86d4FF71D656
+- SolnSquareVerifier - https://rinkeby.etherscan.io/address/0x652Ce41eB9e87DB8E60ba26535E412Fc3Bc08D8f
+
+# Contract ABI
+All contract API are in the build folder
+
+# OpenSea MarketPlace Storefront
+https://rinkeby.opensea.io/assets/manishpropertiesv10
+
 # Project Resources
 
 * [Remix - Solidity IDE](https://remix.ethereum.org/)
--- project/eighth/eth-contracts/test/TestSolnSquareVerifier.js
+++ project/eighth/eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,52 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+const Verifier = artifacts.require('Verifier');
+const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+const proof = require('../../zokrates/code/square/proof.json');
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+contract('SolnSquareVerifier', accounts => {
+    const account_one = accounts[0];
+    const A = proof["proof"]["A"];
+    const A_p = proof["proof"]["A_p"];
+    const B = proof["proof"]["B"];
+    const B_p = proof["proof"]["B_p"];
+    const C = proof["proof"]["C"];
+    const C_p = proof["proof"]["C_p"];
+    const H = proof["proof"]["H"];
+    const K = proof["proof"]["K"];
+    const correctProofInput = proof["input"];
+    let propertyName = "Manish Properties";
+    let propertySymbol = "*";
+    let propertyBaseURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
+
+    describe('match SolnSquareVerifier spec', function () {
+        beforeEach(async function () {
+            this.VerifierContract = await Verifier.new({from: account_one});
+            this.SolnSquareVerifierContract = await SolnSquareVerifier.new(this.VerifierContract.address, propertyName, propertySymbol, propertyBaseURI, {from: account_one});
+        });
+
+        // Test if a new solution can be added for contract - SolnSquareVerifier
+        it('Test if a new solution can be added for contract', async function () {
+            let txObject = await this.SolnSquareVerifierContract.addSolution(A, A_p, B, B_p, C, C_p, H, K, correctProofInput, account_one);
+            let event = txObject.logs[0].event;
+            assert.equal("solutionAdded", event, "Unable to add new solution");
+        });
+
+        it('Test if only unique solutions can be added for contract', async function () {
+            await this.SolnSquareVerifierContract.addSolution(A, A_p, B, B_p, C, C_p, H, K, correctProofInput, account_one);
+            let isUniqueSolution = true;
+            try {
+                await this.SolnSquareVerifierContract.addSolution(A, A_p, B, B_p, C, C_p, H, K, correctProofInput, account_one);
+            } catch (error) {
+                isUniqueSolution = false;
+            }
+            assert.equal(isUniqueSolution, false, "Only unique solutions can be added");
+        });
+
+        // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+        it('Test if an ERC721 token can be minted for contract', async function () {
+            let txObject = await this.SolnSquareVerifierContract.mintNewToken(A, A_p, B, B_p, C, C_p, H, K, correctProofInput, account_one, 1);
+            let event = txObject.logs[1].event;
+            assert.equal("Transfer", event, "Unable to mint a new token");
+        });
+
+    });
+});
